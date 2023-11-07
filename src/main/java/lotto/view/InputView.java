b18950174666fb4548fedb5eb8268118267d9991
이두현 <ldh9411@hanmail.net>
2023-11-08 03:13:20 +0900

    Feat: view 추가
    - 입력 관리를 위한 inputview 추가
    - 출력 관리를 위한 outputview 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,2 +1,20 @@
-package lotto.view;public class InputView {
+package lotto.view;
+
+public class InputView {
+
+    public static final String LOTTO_PURCHASE_MESSAGE = "구입금액을 입력해 주세요.";
+    public static final String LOTTO_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.";
+    public static final String LOTTO_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
+
+    public static void inputLottoPurchaseMessage() {
+        System.out.println(LOTTO_PURCHASE_MESSAGE);
+    }
+
+    public static void inputLottoNumbersMessage() {
+        System.out.println(LOTTO_NUMBERS_MESSAGE);
+    }
+
+    public static void inputLottoBonusNumberMessage() {
+        System.out.println(LOTTO_BONUS_NUMBER_MESSAGE);
+    }
 }
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,2 +1,54 @@
-package lotto.view;public class OutputView {
+package lotto.view;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class OutputView {
+    public static final String LOTTO_PURCHASE_FINISHED_MESSAGE = "개를 구매했습니다.";
+    public static final String LOTTO_STATISTICS_MESSAGE = "당첨 통계\n---\n";
+    public static final String LOTTO_FIFTH_PLACE_MESSAGE = "3개 일치 (5,000원) - %d개";
+    public static final String LOTTO_FOURTH_PLACE_MESSAGE = "4개 일치 (50,000원) - %d개";
+    public static final String LOTTO_THIRD_PLACE_MESSAGE = "5개 일치 (1,500,000원) - %d개";
+    public static final String LOTTO_SECOND_PLACE_MESSAGE = "5개 일치, 보너스 볼 일치 (30,000,000원) - %d개";
+    public static final String LOTTO_FIRST_PLACE_MESSAGE = "6개 일치 (2,000,000,000원) - %d개";
+
+    public static void printLottoPurchaseFinishedMessage(int num) {
+        System.out.println(num + LOTTO_PURCHASE_FINISHED_MESSAGE);
+    }
+
+    public static void printPurchasedLottoNumbers(List<Integer> lottoNumbers) {
+
+        String joinedNumbers = lottoNumbers.stream()
+                .map(String::valueOf)
+                .collect(Collectors.joining(" , "));
+
+        System.out.println("[" + joinedNumbers + " , " + "]");
+    }
+
+    public static void printStaticsMessage() {
+        System.out.println(LOTTO_STATISTICS_MESSAGE);
+    }
+
+    public static void printFifthPlaceResultMessage(int num) {
+
+        System.out.println(String.format(LOTTO_FIFTH_PLACE_MESSAGE, num));
+
+    }
+
+    public static void printFourthPlaceResultMessage(int num) {
+        System.out.println(String.format(LOTTO_FOURTH_PLACE_MESSAGE, num));
+    }
+
+    public static void printThirdPlaceResultMessage(int num) {
+        System.out.println(String.format(LOTTO_THIRD_PLACE_MESSAGE, num));
+    }
+
+    public static void printSecondPlaceResultMessage(int num) {
+        System.out.println(String.format(LOTTO_SECOND_PLACE_MESSAGE, num));
+    }
+
+    public static void printFirstPlaceResultMessage(int num) {
+        System.out.println(String.format(LOTTO_FIRST_PLACE_MESSAGE, num));
+    }
+
 }
