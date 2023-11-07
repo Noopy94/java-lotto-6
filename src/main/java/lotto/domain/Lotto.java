de809490faddf27e3ca4decd86f832bb332caeec
이두현 <ldh9411@hanmail.net>
2023-11-08 02:35:17 +0900

    Feat: domain 추가
    - Lotto 게임을 위한 Lotto class 추가
    - Lotto 게임 숫자 6가지를 받는 LottoNumber class 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,2 +1,38 @@
-package lotto.domain;public class Lotto {
+package lotto.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lotto {
+
+    private List<Integer> lottoWinningNumbers;
+    private int lottoBonusNumber;
+    private int lotteryPurchaseNumber;
+
+    public Lotto() {
+    }
+
+    public List<Integer> getLottoWinningNumbers() {
+        return lottoWinningNumbers;
+    }
+
+    public int getLottoBonusNumber() {
+        return lottoBonusNumber;
+    }
+
+    public int getLotteryPurchaseNumber() {
+        return lotteryPurchaseNumber;
+    }
+
+    public void setLottoWinningNumbers(List<Integer> lottoWinningNumbers) {
+        this.lottoWinningNumbers = lottoWinningNumbers;
+    }
+
+    public void setLottoBonusNumber(int lottoBonusNumber) {
+        this.lottoBonusNumber = lottoBonusNumber;
+    }
+
+    public void setLotteryPurchaseNumber(int lotteryPurchaseNumber) {
+        this.lotteryPurchaseNumber = lotteryPurchaseNumber;
+    }
 }
--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -1,2 +1,25 @@
-package lotto.domain;public class LottoNumber {
+package lotto.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+
+public class LottoNumber {
+
+    private List<Integer> lottoNumbers;
+
+    public LottoNumber() {
+    }
+
+    public List<Integer> getLottoNumbers() {
+        return lottoNumbers;
+    }
+
+    public void setLottoNumbers(List<Integer> lottoNumbers) {
+        this.lottoNumbers = lottoNumbers;
+    }
+
+    public void setRandomLottoNumbers() {
+        this.lottoNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+    }
+
 }
